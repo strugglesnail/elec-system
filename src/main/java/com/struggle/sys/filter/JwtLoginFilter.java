ca3e85dd1d61a8c343324ad1ad8d6dd1ec7c616c
strugglesnail <593616590@qq.com>
2021-02-20 06:39:45 +0800

    添加菜单功能

--- src/main/java/com/struggle/sys/filter/JwtLoginFilter.java
+++ src/main/java/com/struggle/sys/filter/JwtLoginFilter.java
@@ -1,11 +1,12 @@
 package com.struggle.sys.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.struggle.sys.common.Constants;
 import com.struggle.sys.common.TokenResponse;
 import com.struggle.sys.model.LoginUser;
+import com.struggle.sys.pojo.SysUser;
 import com.struggle.sys.security.LoginAuthenticationEntryPoint;
 import com.struggle.sys.service.RedisService;
+import com.struggle.sys.service.SysUserService;
 import com.struggle.sys.util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -24,17 +25,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Date;
 
 /**
  * @auther strugglesnail
  * @date 2021/1/25 23:01
  * @desc Jwt登录过滤器
  */
-//@Component
 public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
 
     @Autowired
     private RedisService redisService;
+
+    @Autowired
+    private SysUserService sysUserService;
 
     private LoginAuthenticationEntryPoint authenticationEntryPoint = new LoginAuthenticationEntryPoint();
 
@@ -58,7 +62,9 @@ public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
         Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
         String name = authResult.getName();
         String[] split = name.split(",");
+        // 用户Id
         Long userId = Long.valueOf(split[0]);
+        // 用户账号
         String account = split[1];
 
         UserDetails userDetails = User.builder().username(name).password("").authorities(authorities).build();
@@ -73,11 +79,19 @@ public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
         redisService.set(JwtUtils.getAccessTokenKey(userId), accessToken);
         redisService.set(JwtUtils.getRefreshTokenKey(userId), refreshToken);
 
+        // 更新用户登录时间
+        updateUserLoginTime(userId);
+
         // 响应登录信息
         JwtUtils.writerToJson(response, TokenResponse.createBySuccessMessage(accessToken, refreshToken, userId));
     }
 
-
+    private void updateUserLoginTime(Long userId) {
+        SysUser user = new SysUser();
+        user.setId(userId);
+        user.setLastLoginTime(new Date());
+        sysUserService.updateUser(user);
+    }
 
 
     // 登录失败返回信息
